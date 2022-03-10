3b37bea6e03340efbb32ab35de20c2464df2eb7f
Vitor <vitor.belarmino@hotmail.com>
2022-03-10 16:49:52 -0300

    requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,23 @@
 export async function getCategories() {
-  // Implemente aqui
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const result = await response.json();
+  return result;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (categoryId && query) {
+    const response = await fetch(` https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+    const result = await response.json();
+    return result;
+  }
+  if (!query) {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+    const result = await response.json();
+    return result;
+  } if (!categoryId) {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+    const result = await response.json();
+
+    return result;
+  }
 }
