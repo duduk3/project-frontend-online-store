5f4cd147a07000266807cd1c5c3a593f378ba408
Vitor <vitor.belarmino@hotmail.com>
2022-03-10 17:31:30 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
 
 export default class App extends Component {
   render() {
