c71b60d22b56a7e82ff5ff3b98ee028deb877934
Vitor <vitor.belarmino@hotmail.com>
2022-03-11 20:34:24 -0300

    requisito 8 completo, falta retirar alguns console.log

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -13,20 +13,22 @@ class Cart extends React.Component {
 
   render() {
     const prevStorage = JSON.parse(localStorage.getItem('cart'));
-    console.log(prevStorage);
-
     return (
       <div>
         <h1>carrinho de compra</h1>
         {prevStorage === null
           ? <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
           : (
-            prevStorage.map((product) => (
-              <div key={ product.title }>
-                <p data-testid="shopping-cart-product-name" />
-                <p data-testid="shopping-cart-product-quantity" />
-              </div>
-            )))}
+            prevStorage.map((product) => {
+              const quantidade = prevStorage.filter((ele) => product.title === ele.title);
+              console.log(quantidade.length);
+              return (
+                <div key={ product.title }>
+                  <p data-testid="shopping-cart-product-name">{product.title}</p>
+                  <p data-testid="shopping-cart-product-quantity">{quantidade.length}</p>
+                </div>
+              );
+            }))}
         {/* <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2> */}
       </div>
 
